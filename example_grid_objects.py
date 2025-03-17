fca8afa93f2f922009e8b1bc1a245ab955e9ef66
cenfraGit <cenfra_ma@hotmail.com>
2025-03-16 23:50:06 -0400

    added more objects

--- example_grid_objects.py
+++ example_grid_objects.py
@@ -23,7 +23,7 @@ class MyObject(NightObject):
 
         # move forward/side respect to camera orientation
 
-        force = 200
+        force = 400
 
         forward = self.camera.get_forward_vector()
         forward = [forward[0], forward[2], 0]
@@ -41,16 +41,16 @@ class MyObject(NightObject):
         if self.check_pressed(window, glfw.KEY_L):
             p.applyExternalForce(self.physics_id, -1, [-v for v in side], self.get_position(True), p.WORLD_FRAME)
         if self.check_pressed(window, glfw.KEY_Y):
-            p.applyExternalForce(self.physics_id, -1, [0, 0, 2000], self.get_position(True), p.WORLD_FRAME)
+            p.applyExternalForce(self.physics_id, -1, [0, 0, 500], self.get_position(True), p.WORLD_FRAME)
         if self.check_pressed(window, glfw.KEY_H):
-            p.applyExternalForce(self.physics_id, -1, [0, 0, -700], self.get_position(True), p.WORLD_FRAME)
+            p.applyExternalForce(self.physics_id, -1, [0, 0, -500], self.get_position(True), p.WORLD_FRAME)
 
 class Example(NightBase):
     def setup(self):
 
         self.scene = self.create_scene()
         self.camera = NightCamera()
-        self.camera.set_position([0, 10, -10])
+        self.camera.set_position([0, 10, -30])
         self.set_gravity(z=-40)
 
         self.grid = ObjectGrid(width=100, divisions=20, color=[0.5, 0.5, 0.5])
@@ -58,22 +58,24 @@ class Example(NightBase):
 
         self.axes = ObjectAxes(length=10, line_width=6)
         self.scene.add(self.axes)
-        
-        self.cube = NightObject(MeshBox(5), NightMaterial(), mass=3)
-        self.cube.set_position([0, 8, 0])
-        self.scene.add(self.cube)
 
         self.sphere = MyObject(self.camera)
-        self.sphere.set_position([0, 40, 0])
+        self.sphere.set_position([0, 10, -20])
         self.scene.add(self.sphere)
+
+        w = 5
+        hor = 5
+        vert = 5
+        for i in range(-hor, hor+1):
+            for j in range(2, vert):
+                cube = NightObject(MeshBox(w), NightMaterial(), mass=1)
+                cube.set_position([i*w, j*w, 0])
+                self.scene.add(cube)
 
     def update(self):
         self.sphere.move(self.window, self.time_delta)
-
         p.changeDynamics(self.sphere.physics_id, -1, restitution=0.9)
-        p.changeDynamics(self.cube.physics_id, -1, restitution=0.3)
         p.changeDynamics(self.grid.physics_id, -1, restitution=0.8)
-        
         self.draw_scene(self.camera)
 
 if __name__ == "__main__":
